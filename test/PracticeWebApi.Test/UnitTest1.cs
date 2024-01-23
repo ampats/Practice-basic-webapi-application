8888093c09fbfa09361b6bc1030769e278850625
Anna-Maria Patsiali <116801308+ampats@users.noreply.github.com>
2024-01-23 18:59:36 +0200

    [DCE-161] Dotnet Web Application Walking Skeleton - Test (#2)
    * Add testing project
    
    * Add testing project to the solution
    
    * Add instructions on how to run the test
    
    * Add assertion to check that tests can fail
    
    * Fix solution file - remove unnecessary lines
    
    * Modify test script to use xUnit testing library

---
+++ test/PracticeWebApi.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+
+namespace PracticeWebApi.Test {
+
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+            int val = 1;
+            Assert.NotEqual(val, 1);        }
+    }
+}
